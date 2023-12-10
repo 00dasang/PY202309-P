b3ba173a16170e88cea9940c8e8b531a14d46a60
00dasang <00dasang@naver.com>
2023-12-10 23:58:39 +0900

    Progress_report_2 edit last

--- Sources/Project_codes/main.py
+++ Sources/Project_codes/main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #module_readlines
 
 #module_finding image
@@ -6,12 +5,3 @@
 #module_write_searching_records_on_new_file
 
 #module_send_email_in_python
-=======
-#module_readlines
-
-#module_finding image
-
-#module_write_searching_records_on_new_file
-
-#module_send_email_in_python
->>>>>>> 33927a444350dc5f48a6133617b73944b9035b93
--- Sources/Project_codes/sending_email.py
+++ Sources/Project_codes/sending_email.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
@@ -32,39 +31,4 @@ with smtplib.SMTP(smtp_server, smtp_port) as server:
     # 이메일 보내기
     server.sendmail(sender_email, receiver_email, message.as_string())
 
-=======
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-import getpass
-
-# 사용자에게 이메일 정보 입력 받기
-sender_email = input("Enter your email address: ")
-sender_password = getpass.getpass("Enter your email password: ")
-receiver_email = input("Enter the recipient's email address: ")
-
-# 이메일 서버 설정 (Gmail의 경우)
-smtp_server = "smtp.gmail.com"
-smtp_port = 587
-
-# 이메일 내용 입력 받기
-subject = input("Enter the subject of the email: ")
-body = input("Enter the body of the email: ")
-
-# MIMEText를 사용하여 이메일 내용 설정
-message = MIMEMultipart()
-message['From'] = sender_email
-message['To'] = receiver_email
-message['Subject'] = subject
-message.attach(MIMEText(body, 'plain'))
-
-# SMTP 서버에 연결 및 로그인
-with smtplib.SMTP(smtp_server, smtp_port) as server:
-    server.starttls()
-    server.login(sender_email, sender_password)
-
-    # 이메일 보내기
-    server.sendmail(sender_email, receiver_email, message.as_string())
-
->>>>>>> 33927a444350dc5f48a6133617b73944b9035b93
 print("이메일이 성공적으로 전송되었습니다.")
